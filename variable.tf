d1c43c2d1622fbb5649d7b7cb255492c79e9751f
govindaraju10 <123375983+govindaraju10@users.noreply.github.com>
2023-10-11 09:41:01 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -24,14 +24,14 @@ resource "aws_instance" "example_instance" {
 #aws listner
 variable "listener" {
   type = object({
-    load_balancer_arn = string
-    port              = number
-    protocol          = string
-    ssl_policy        = string
-    certificate_arn   = string
+    load_balancer_arn = aws_lb.front_end.arn
+    port              = "443"
+    protocol          = "HTTPS"
+    ssl_policy        = "ELBSecurityPolicy-2016-08"
+    certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
     default_action    = object({
-      type             = string
-      target_group_arn = string
+      type             = "forward"
+      target_group_arn = aws_lb_target_group.front_end.arn
     })
   })
 }
